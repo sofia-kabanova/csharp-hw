d3e58f27c399a2439ce9a77457ca2cd0c33c8776
Sofia Kabanova <sofia.kabanova@gmail.com>
2022-09-19 03:19:23 +0300

    done 04

--- 04/Program.cs
+++ 04/Program.cs
@@ -11,5 +11,5 @@ int num3 = Convert.ToInt32(Console.ReadLine());
 int max=num1;
 if(num2>max ) max=num2;
 if(num3>max ) max=num3;
-Console.Write("max = ");
+Console.Write("max=");
 Console.WriteLine(max);
