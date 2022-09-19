5f9ea3d94a175b7df5243d83ed07b896d9934004
Sofia Kabanova <sofia.kabanova@gmail.com>
2022-09-19 03:18:41 +0300

    done 02

--- 02/Program.cs
+++ 02/Program.cs
@@ -2,9 +2,9 @@
 // на вход принимает два числа
 //  и выдаёт, какое число большее, а какое меньшее.
 Console.Clear();
-Console.Write("Введите целое число1: ");
+Console.Write("Введите целое число 1: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите целое число2: ");
+Console.Write("Введите целое число 2: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 if(num1>num2)
 {
